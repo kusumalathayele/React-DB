22f8b43ef21a136da7784ab2b1cec6875c1e55e2
kusumalathayele <kusumalathayele@gmail.com>
2024-03-12 15:08:26 +0530

    kusuma

--- src/components/serachbar.js
+++ src/components/serachbar.js
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 
 function SearchBar({ onSearch }) {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
 
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
@@ -11,19 +11,13 @@ function SearchBar({ onSearch }) {
   const handleSubmit = (event) => {
     event.preventDefault();
     onSearch(searchTerm);
-  };
+  }
 
   return (
     <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={searchTerm}
-        onChange={handleChange}
-        placeholder="Search for a movie..."
-      />
-      <button type="submit">Search</button>
+      <input type="text" value={searchTerm} onChange={handleChange} placeholder="Search for a movie..." style={{backgroundColor:"lightblue"}}/>
+      <button type="submit" style={{backgroundColor:"lightpink"}}>Search</button>
     </form>
-  );
+  )
 }
-
 export default SearchBar;
